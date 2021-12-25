bb4c621dd3c4602348461fe8f6ba0252e990f2fb
Max Heiber <max.heiber@gmail.com>
2021-12-25 15:44:21 +0000

    make it a synth (#6)
    * refactors: fewer '.clone()'s, general draw
    
    - Make cloning more implicit because it was annoying.
    - Draw a vector of signals rather than shuttling around data
    for each signal individually. This will make things easier
    when we visualize more than just sine+square+combined.
    
    * remove dead code
    
    * noop-move waves to separate module
    
    * make it a synth
    
    Use `cargo run` to see directions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use std::f64::consts::PI;
 use std::fmt::Debug;
 use std::sync::Arc;
 #[macro_use]
 extern crate custom_derive;
 #[macro_use]
 extern crate newtype_derive;
+
+pub mod standard_signals;
 
 custom_derive! {
     #[derive(Debug, PartialEq, Clone, PartialOrd, Copy, Default, NewtypeFrom, NewtypeAdd, NewtypeSub, NewtypeMul, NewtypeDiv)]
@@ -19,6 +20,8 @@ custom_derive! {
 #[derive(Clone)]
 pub struct Signal {
     f: Arc<dyn Fn(TimeSecs) -> f64 + Send + Sync + 'static>,
+    /// used for increasing frequency
+    mul_input: f64,
     /// used for .phase()
     add_input: f64,
     /// used for .scale()
@@ -27,6 +30,7 @@ pub struct Signal {
 
 impl Debug for Signal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("signal").unwrap();
         f.write_str("signal")
     }
 }
@@ -44,27 +48,39 @@ impl std::ops::Add for Signal {
 }
 
 impl Signal {
+    pub fn sum(signals: Vec<Signal>) -> Signal {
+        let f = move |t: TimeSecs| -> f64 { signals.iter().map(|s| s.at(t)).sum() };
+        Self::new(Arc::new(f))
+    }
+
     pub fn new(f: Arc<dyn Fn(TimeSecs) -> f64 + Send + Sync + 'static>) -> Self {
         Self {
             f,
+            mul_input: 1.0,
             add_input: 0.0,
             mul_output: 1.0,
         }
     }
-    pub fn scale(self, by: f64) -> Signal {
+    pub fn scale(&self, by: f64) -> Signal {
         Self {
             mul_output: self.mul_output * by,
-            ..self
+            ..self.clone()
         }
     }
-    pub fn phase(self, by: f64) -> Signal {
+    pub fn incr_frequency(&self, by: f64) -> Signal {
+        Self {
+            mul_input: self.mul_input * by,
+            ..self.clone()
+        }
+    }
+    pub fn phase(&self, by: f64) -> Signal {
         Self {
             add_input: self.add_input + by,
-            ..self
+            ..self.clone()
         }
     }
     pub fn at(&self, time: TimeSecs) -> f64 {
-        (self.f)(time + TimeSecs(self.add_input)) * self.mul_output
+        (self.f)(TimeSecs(self.mul_input * time.0 + self.add_input)) * self.mul_output
     }
 }
 
@@ -78,45 +94,10 @@ impl FrequencyHz {
     }
 }
 
-pub fn sine_wave(freq: FrequencyHz) -> Signal {
-    let f = move |t| (2.0 * PI * freq.at(t)).sin();
-    Signal::new(Arc::new(f))
-}
-
-pub fn square_wave(freq: FrequencyHz) -> Signal {
-    let f = move |t: TimeSecs| {
-        let is_even = (freq.at(t).round() as u32) % 2 == 0;
-        if is_even {
-            -1.0
-        } else {
-            1.0
-        }
-    };
-    Signal::new(Arc::new(f))
-}
-
 pub fn sample(rate: FrequencyHz, s: Signal) -> impl Iterator<Item = f64> {
     (0..).map(move |n: u32| {
         let sample_period = rate.period();
-        let t = (n as f64) * (sample_period.0);
+        let t = n as f64 * sample_period.0;
         s.at(TimeSecs(t))
     })
-}
-
-// stuff below this line is currently unused
-pub struct Weight(pub u32);
-
-custom_derive! {
-    #[derive(Debug, PartialEq, PartialOrd, Clone, Copy, NewtypeAdd, NewtypeSub, NewtypeDiv, NewtypeMul, NewtypeFrom)]
-    pub struct Connection(pub f64);
-}
-
-pub fn weighted_sum(ws: Vec<Weight>, cs: Vec<Connection>) -> f64 {
-    assert_eq!(ws.len(), cs.len());
-    ws.iter().zip(cs).map(|(w, c)| (w.0 as f64) * c.0).sum()
-}
-
-pub fn run_weights(ws: Vec<Weight>, cs: Vec<Connection>) -> Connection {
-    let negated_cs = cs.iter().map(|c| Connection(-c.0));
-    Connection(weighted_sum(ws, negated_cs.collect()))
 }
